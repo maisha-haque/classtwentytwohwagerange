2829fc977beda563321d16bcd7d8111803dd2332
maisha-haque <maishaireen@gmail.com>
2026-05-11 01:37:51 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+let age = 18;
+
+if (age >= 1 && age <= 3){
+    console.log("You're a toddler.");
+    
+}
+else if (age >= 4 && age <= 12){
+    console.log("You're a child.");
+    
+}
+else if (age >= 13 && age <= 19){
+    console.log("You're a teenager.");
+    
+}
+else if (age >= 20 && age <= 39){
+    console.log("You're a young-adult.");
+    
+}
+else if (age >= 40 && age <= 60){
+    console.log("You're a middle-aged adult.");
+    
+}
+else if (age >= 61 && age <= 89){
+    console.log("You're a senior citizen.");
+    
+}
+else if (age >= 90 && age <= 110){
+    console.log("Sadly you are gonna die soon. Rip in advance.");
+    
+}
+else if (age >= 111 && age <= 150){
+    console.log("Rip.");
+    
+}
+else {
+    console.log("Enter your correct age.");
+    
+}
